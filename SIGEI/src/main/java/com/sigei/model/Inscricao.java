6bb891d5b0daea120214487e5b1f3dbebf35a247
GabrielSilvaVG <gabrieldepaulajf@gmail.com>
2025-06-08 10:30:18 -0300

    Classe Inscricao

---
+++ SIGEI/src/main/java/com/sigei/model/Inscricao.java
@@ -0,0 +1,65 @@
+package com.sigei.model;
+
+import com.sigei.model.enm.EStatusInscricao;
+
+import java.time.LocalDateTime;
+
+public class Inscricao {
+    private int id;
+    private Evento evento;
+    private Participante participante;
+    private LocalDateTime dataInscricao;
+    private EStatusInscricao statusInscricao;
+
+
+    public Inscricao(Evento evento, Participante participante, EStatusInscricao statusInscricao, LocalDateTime dataInscricao) {
+        this.evento = evento;
+        this.participante = participante;
+        this.statusInscricao = statusInscricao;
+        this.dataInscricao = dataInscricao;
+    }
+
+    // Getters e Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public EStatusInscricao getStatusInscricao() {
+        return statusInscricao;
+    }
+
+    public void setStatusInscricao(EStatusInscricao statusInscricao) {
+        this.statusInscricao = statusInscricao;
+    }
+
+    public Participante getParticipante() {
+        return participante;
+    }
+
+    public void setParticipante(Participante participante) {
+        this.participante = participante;
+    }
+
+    public Evento getEvento() {
+        return evento;
+    }
+
+    public void setEvento(Evento evento) {
+        this.evento = evento;
+    }
+
+    public LocalDateTime getDataInscricao() {
+        return dataInscricao;
+    }
+
+    public void setDataInscricao(LocalDateTime dataInscricao) {
+        this.dataInscricao = dataInscricao;
+    }
+
+
+}
